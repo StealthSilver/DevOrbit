71bf1a48a7195183969720cc433534909d25ffd8
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-09-28 13:08:58 +0530

    backend init

--- backend/src/config/aws-confing.ts
+++ backend/src/config/aws-confing.ts
@@ -1,8 +1,8 @@
 import AWS from "aws-sdk";
 
-AWS.config.update({ region: "ap-south-1" });
+AWS.config.update({ region: "eu-north-1" });
 
 const s3: AWS.S3 = new AWS.S3();
-const S3_BUCKET: string = "insert_bucket_name";
+const S3_BUCKET: string = "sampledevorbitbucket";
 
 export { s3, S3_BUCKET };
