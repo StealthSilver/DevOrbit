06d28e59f47bbf37eec0d81165b229410fae20e2
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-09-28 13:26:52 +0530

    aws config

--- backend/src/config/aws-confing.ts
+++ backend/src/config/aws-confing.ts
@@ -1,8 +1,15 @@
+import dotenv from "dotenv";
 import AWS from "aws-sdk";
 
-AWS.config.update({ region: "eu-north-1" });
+dotenv.config();
 
-const s3: AWS.S3 = new AWS.S3();
-const S3_BUCKET: string = "sampledevorbitbucket";
+AWS.config.update({
+  region: process.env.AWS_REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
+
+const s3 = new AWS.S3();
+const S3_BUCKET = "sampledevorbitbucket";
 
 export { s3, S3_BUCKET };
